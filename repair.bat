7d905a3278dbce1e2f6fecb20f0fcc8bbc03d79d
AdasAdasiek <75744132+AdasAdasiek@users.noreply.github.com>
2022-09-10 21:58:32 +0200

    change link

--- repair.bat
+++ repair.bat
@@ -13,6 +13,6 @@ echo Usuwanie pliku RailRoads.exe
 del RailRoads.exe
 echo Usunieto
 echo Pobieranie naprawionej wersji.
-curl https://github.com/AdasAdasiek/railroads-repair/releases/download/release/RailRoads.exe -o RailRoads.exe
+curl https://github.com/AdasAdasiek/railroads-repair/raw/main/files/RailRoads.exe -o RailRoads.exe
 echo Pomyslnie naprawiono railroads.
 pause >nul
